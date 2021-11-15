091d9aa14fea50403abe76d407d64890c24fb253
Michael Guanoluisa <michael.guanoluisa@epn.edu.ec>
2021-11-15 17:29:16 -0500

    error

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -16,7 +16,7 @@ const Events = require("../models/events");
 //
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, __dirname + "../../../uploads");
+    cb(null, "uploads");
   },
   filename: (req, file, cb) => {
     const ext = file.originalname.split(".").pop();
