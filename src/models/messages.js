9ab9c509e9f1994daba78ecc3a47f096de0914ed
Michael Guanoluisa <michael.guanoluisa@epn.edu.ec>
2021-11-15 02:18:12 -0500

    modelo de noticias y mensajes

--- src/models/messages.js
+++ src/models/messages.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const MessagesScheme = new mongoose.Schema({
-        name:{
+        title:{
             type: String,
             required: true
         },
