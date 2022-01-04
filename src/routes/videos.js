c0b74dac77677767df0f39672e272a71a0e7e2a4
MichaelAndrik <michaelandrik10@gmail.com>
2022-01-04 13:20:47 -0500

    fix: routes videos

--- src/routes/videos.js
+++ src/routes/videos.js
@@ -2,16 +2,10 @@ const express = require("express");
 const router = express.Router();
 const videosCtrl = require("../controllers/videosController");
 const { verifyToken, isAdmin } = require("../middlewares/authorization");
-const { validateCreate } = require("../validators/videos");
 
 router.get("/", videosCtrl.getVideos);
 router.get("/:id", videosCtrl.getVideosById);
-router.post(
-  "/",
-  [verifyToken, isAdmin],
-  validateCreate,
-  videosCtrl.createVideos
-);
+router.post("/", [verifyToken, isAdmin], videosCtrl.createVideos);
 router.put("/:id", [verifyToken, isAdmin], videosCtrl.updateVideosById);
 router.delete("/:id", [verifyToken, isAdmin], videosCtrl.deleteVideosById);
 
