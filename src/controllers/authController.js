612197ef2a03fc2bd04790065da57c8c792f5c97
MichaelAndrik <michaelandrik10@gmail.com>
2022-01-06 18:56:03 -0500

    auth-status code

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,9 +2,8 @@ const User = require("../models/users");
 const Role = require("../models/role");
 const jwt = require("jsonwebtoken");
 const res = require("express/lib/response");
-const {httpError} = require("../helpers/handleError")
+const { httpError } = require("../helpers/handleError");
 //const config = require('../config');
-
 
 exports.register = async (req, res) => {
   //obtener datos
@@ -12,9 +11,8 @@ exports.register = async (req, res) => {
 
   try {
     const user = await User.findOne({ email: email });
-    
-    if(!user){
 
+    if (!user) {
       const newUser = new User({
         name,
         lastname,
@@ -29,28 +27,24 @@ exports.register = async (req, res) => {
         const role = await Role.findOne({ name: "user" });
         newUser.roles = [role._id];
       }
-    
+
       //guardar usuario
       await newUser.save();
       res.status(200).send({ message: "Usuario registrado con exito" });
-    }else{
-      res.send({message: "Este usuario ya esta registrado"})
+    } else {
+      res.send({ message: "Este usuario ya esta registrado" });
     }
-    
   } catch (e) {
     httpError(res, e);
   }
-
 };
 
 exports.login = async (req, res) => {
-
   try {
-    
     const user = await User.findOne({ email: req.body.email });
 
-    if (!user){
-      return res.status(404).send({message: "usuario no encontrado" });
+    if (!user) {
+      return res.status(404).send({ message: "usuario no encontrado" });
     }
 
     const roles = await Role.find({ _id: { $in: user.roles } });
@@ -61,7 +55,7 @@ exports.login = async (req, res) => {
       user.password
     );
 
-    if (!matchPassword){
+    if (!matchPassword) {
       return res.status(401).send({ message: "Contraseña incorrecta" });
     }
 
@@ -69,13 +63,9 @@ exports.login = async (req, res) => {
       expiresIn: 86400, //24h
     });
 
-
-    return res.send(
-      { token: req.token,
-        user: user,
-      role: roleName}
-    );
-
+    return res
+      .status(200)
+      .send({ token: req.token, user: user, role: roleName });
   } catch (e) {
     httpError(res, e);
   }
@@ -87,43 +77,37 @@ exports.me = async (req, res) => {
     const token = req.headers["x-access-token"];
 
     //extraer datos del token
-    const decoded = this.decoded(token)
+    const decoded = this.decoded(token);
 
     //comprobar si el usuario existe
     const user = await User.findById(decoded.id, { password: 0 });
-    if (!user){
+    if (!user) {
       return res.status(404).json({ message: "usuario no activo" });
-    }else{
-      return res.send({user: user})
+    } else {
+      return res.send({ user: user });
     }
- 
   } catch (error) {
     return res.status(401).json({ message: "token invalido" });
   }
-  
-}
+};
 
 exports.logout = (req, res) => {
-  
   try {
-
     const token = req.headers["x-access-token"];
-    if(!token) return res.status(403).send({ message: "Inicie sesión" });
+    if (!token) return res.status(403).send({ message: "Inicie sesión" });
     res.status(200).send({ msg: "logged out" });
-
   } catch (error) {
-    return res.status(500).send({message: error})
+    return res.status(500).send({ message: error });
   }
-}
+};
 
 exports.decoded = (token) => {
   try {
-    if (!token){
+    if (!token) {
       return res.status(403).send({ message: "Inicie sesión" });
     }
-    return decoded = jwt.verify(token, process.env.SECRET_KEY);
+    return (decoded = jwt.verify(token, process.env.SECRET_KEY));
   } catch (error) {
     httpError(res, e);
-  } 
-  
-}
+  }
+};
