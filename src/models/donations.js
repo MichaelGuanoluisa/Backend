1e32f57890d33add9bf3c9bbc0b96c4597e68de6
MichaelAndrik <michaelandrik10@gmail.com>
2022-01-06 10:57:25 -0500

    correcting existing errors

--- src/models/donations.js
+++ src/models/donations.js
@@ -1,37 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const DonationsScheme = new mongoose.Schema(
-    {
-      id_user: {
-        ref: "users",
-        type: mongoose.Schema.Types.ObjectId
-      },
-      description: {
-        type: String,
-        required: true
-      },
-      type: {
-          type: String,
-          required: true
-      },
-      delivery: {
-          type: String,
-      },
-      direction: {
-        type: String
-      },
-      dateDelivery: {
-        type: String,
-        required: true
-      },
-      imgURL: {
-        type: String
-      }
+  {
+    id_user: {
+      ref: "users",
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
-    {
-      versionKey: false,
-      timestamps: true,
-    }
-  );
-  
-  module.exports = mongoose.model("donation", DonationsScheme);
+    description: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    delivery: {
+      type: String,
+      required: false,
+    },
+    direction: {
+      type: String,
+      required: false,
+    },
+    dateDelivery: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Number,
+      required: true,
+    },
+    imgURL: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("donation", DonationsScheme);
