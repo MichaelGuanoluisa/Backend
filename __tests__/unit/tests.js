71e7e977b37388abc9c9440754f2de1c73a1f201
MichaelAndrik <michaelandrik10@gmail.com>
2022-01-21 11:54:01 -0500

    test: creating tests for register

--- __tests__/unit/tests.js
+++ __tests__/unit/tests.js
@@ -20,6 +20,24 @@ it("Post login respond a json with contain a user", (done) => {
     .end(done);
 });
 
+it("Post register respond a json with message user registered", (done) => {
+  data = {
+    name: "michael",
+    lastname: "guanoluisa",
+    email: `${faker.name.firstName()}+ @hotmail.com`,
+    cellphone: "0961393801",
+    password: "12341234",
+  };
+  request(app)
+    .post("/api/auth/register")
+    .send(data)
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .expect('{"message":"Usuario registrado con exito"}')
+    .end(done);
+});
+
 it("Get News respond a json with contain news", (done) => {
   request(app)
     .get("/api/news")
@@ -29,7 +47,7 @@ it("Get News respond a json with contain news", (done) => {
 });
 
 it("Get a news by id respond 404 not found", (done) => {
-  id = mongoose.Types.ObjectId(faker.datatype.string());
+  id = "61c10af1c76fd906ecba0634";
   request(app)
     .get(`/api/news/${id}`)
     .set("Accept", "application/json")
@@ -63,7 +81,10 @@ it("Post news with a json", (done) => {
     .post("/api/news")
     .send(data)
     .set("Accept", "application/json")
-    .set("x-access-token", token)
+    .set(
+      "x-access-token",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYWU0ZDIwMGU3YzYwMDgzMzM4YzhhMiIsImlhdCI6MTY0MjY5OTUwOSwiZXhwIjoxNjQyNzg1OTA5fQ.CXUHbMfBECFYroIYa01dPcNYbGKcjU9h2x75FvxeK8A"
+    )
     .expect("Content-Type", /json/)
     .expect(201)
     .end(done);
