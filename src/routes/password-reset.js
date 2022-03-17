f4eb6c6c4b4d0dfe541b7c42a6408dae91a86fe5
MichaelAndrik <michael.guanoluisa@epn.edu.ec>
2022-03-16 20:17:47 -0500

    fix: password-reset-id

--- src/routes/password-reset.js
+++ src/routes/password-reset.js
@@ -1,9 +1,15 @@
 const User = require("../models/users");
+const mongoose = require("mongoose");
 const sendEmail = require("../libs/sendEmail");
 //const Joi = require("joi");
 const validations = require("../validators/password-reset")
 const express = require("express");
 const router = express.Router();
+
+
+const parseId = (id) => {
+  return mongoose.Types.ObjectId(id);
+};
 
 router.post("/", async (req, res) => {
   try {
@@ -36,21 +42,23 @@ router.post("/:userId", async (req, res) => {
     // const schema = Joi.object({ password: Joi.string().required() });
     // const { error } = schema.validate(req.body);
     // if (error) return res.status(400).send(error.details[0].message);
+    const id = req.params.userId
     const errors = validations.validatePassword(req);
     if (errors) {
       return res.status(406).send(errors);
     } else {
-      const user = await User.findById(req.params.userId);
+      const user = await User.findById(id);
       if (!user) return res.status(400).send("Link inválido o caducado");
 
-      user.password = req.body.password;
-      await user.save();
+      const password = await User.encryptPassword(req.body.password);
+      req.body.password = password;
 
+      await User.updateOne({ _id: parseId(id) }, req.body); 
       res.status(200).send("Contraseña actualizada con éxito");
     }
   } catch (error) {
     console.log(error);
-    res.status(503).send("Ha ocurrido un error :C :", error.message);
+    res.status(503).send(`Ha ocurrido un error :C : ${error.message}`);
   }
 });
 
